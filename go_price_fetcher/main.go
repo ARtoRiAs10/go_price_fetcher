0bc891786c8f24ecdd30831f7b1ac1f8857a9c30
Gaurav kumar <121500265+ARtoRiAs10@users.noreply.github.com>
2024-11-24 18:39:22 +0530

    Add files via upload

---
+++ go_price_fetcher/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/ARtoRiAs10/client"
+)
+
+func main() {
+
+	client := client.New("http://localhost:3000")
+
+	price, err := client.FetchPrice(context.Background(), "ET")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+V\n", price)
+
+	return
+	listenAddr := flag.String("listenaddr", ":3000", "listen address server")
+	flag.Parse()
+	svc := NewLloggingService(NewMetricService(&priceFetcher{}))
+
+	server := NewJSONAPIServer(*listenAddr, svc)
+	server.Run()
+
+	// price, err := svc.FetchPrice(context.Background(), "ETH")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(price)
+
+}
